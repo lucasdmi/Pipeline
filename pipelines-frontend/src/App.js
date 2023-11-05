95d2075817a34875b935b2353cadcdcdf91b5fa7
lucasdmi <lucasmisidoro>
2023-11-04 22:55:55 -0300

    trabalho 3DVPR - Build an Deploy

---
+++ pipelines-frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Bem-vindo à minha aplicação!</h1>
+        <p>Este é um exemplo simples de frontend.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
